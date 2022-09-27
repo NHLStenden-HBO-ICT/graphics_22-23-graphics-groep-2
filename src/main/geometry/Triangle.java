5acbb37bf7e69d8df61277e82bee3b0796cec20c
kajatrijsburg <kajatrijsburg@gmail.com>
2022-09-27 16:30:26 +0200

    added getters and setters for Triangle

--- src/main/geometry/Triangle.java
+++ src/main/geometry/Triangle.java
@@ -4,22 +4,38 @@ import main.maths.Vector3;
 
 public class Triangle extends Solid {
 
-	private Vector3[] Vertices;
+    private Vector3[] Vertices;
 
-	public Vector3 getvertex(int index) {
-		return null;
-	}
+    public Triangle(Vector3 point1, Vector3 point2, Vector3 point3) {
+        Vertices = new Vector3[]{
+                point1,
+                point2,
+                point3,
+        };
+    }
 
-	public Vector3[] getvertices() {
-		return null;
-	}
+    public Vector3 getvertex(int index) {
+        return Vertices[index];
+    }
 
-	public void setvertex(int index, Vector3 vertex) {
+    public Vector3[] getvertices() {
+        return Vertices;
+    }
 
-	}
+    public void setvertex(int index, Vector3 vertex) {
+        this.Vertices[index] = vertex;
+    }
 
-	public void setvertices(Vector3[] vertices) {
+    //this method would normally allow us to change the length of the Triangle.Vertices array
+    //to prevent this we need to ensure the correct length of the provided array
+    public void setVertices(Vector3[] vertices) {
+        if (vertices.length != 3) {
+            throw new RuntimeException(
+                    "A triangle can only have 3 vertices but " + vertices.length + " were provided");
+        }
 
-	}
+        this.Vertices = vertices;
+    }
+
 
 }
---
+++ src/test/geometry/TriangleTest.java
@@ -0,0 +1,49 @@
+package test.geometry;
+
+import main.geometry.Triangle;
+import main.maths.Vector3;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTest {
+    //these tests ensure that Triangle.setVertices does not allow us to change the length of the vertex array
+
+    Triangle triangle;
+
+    @BeforeEach
+    void setUp() {
+        //make a new triangle before each test
+        triangle = new Triangle(
+                new Vector3(0, 0, 0),
+                new Vector3(0, 0, 0),
+                new Vector3(0, 0, 0));
+    }
+
+    @Test
+    void setVertices() {
+        //make a vertex array of the correct length
+        Vector3[] vecs = {new Vector3(1, 1, 1),
+                new Vector3(1, 1, 1),
+                new Vector3(1, 1, 1)};
+
+        //assign that vertex array to the triangle
+        triangle.setVertices(vecs);
+
+        //check if the triangle is changed as we expect it to
+        assertEquals(1, triangle.getvertex(0).GetX());
+    }
+
+    @Test
+    void setVerticesWrong() {
+        //make a vertex array of incorrect length
+        Vector3[] vecs = {new Vector3(1, 1, 1),
+                new Vector3(1, 1, 1)};
+
+        //() -> is a lambda expression, it's required to use assertThrows
+        //This statement asserts that SetVertices correctly errors when
+        //we try to assign an incorrect number of vertices to the triangle
+        assertThrows(RuntimeException.class, () -> triangle.setVertices(vecs));
+    }
+}
