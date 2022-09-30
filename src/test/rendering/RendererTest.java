e08c5f40d0cc025389d10b6e147ab522b5a8d327
Job Noordenbos <job.noordenbos@student.nhlstenden.com>
2022-09-30 11:14:15 +0200

    Added test files for rendering

---
+++ src/test/rendering/RendererTest.java
@@ -0,0 +1,25 @@
+package test.rendering;
+
+import main.scene.Scene;
+
+public class RendererTest {
+
+    private Scene scene;
+
+    private int maxRayDepth;
+
+    public FrameTest renderFrame(Scene scene, int maxRayDepth) {
+        return null;
+    }
+
+	// Return the current scene
+	public Scene getscene() {
+		return scene;
+	}
+
+	// Set the current scene
+	public void setscene(Scene scene_) {
+		scene = scene_;
+	}
+
+}
