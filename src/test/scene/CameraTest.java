c2ceeec1c985f142c92239ec5111871645f2bfdd
Job Noordenbos <job.noordenbos@student.nhlstenden.com>
2022-09-30 16:47:13 +0200

    Added getters and setters to CameraTest

--- src/test/scene/CameraTest.java
+++ src/test/scene/CameraTest.java
@@ -10,21 +10,32 @@ public class CameraTest {
 
     // Return camera position
     public Vector3 getPosition() {
-        return null;
+        return position;
     }
 
     // Return camera look direction
     public Vector3 getDirection() {
-        return null;
+        return direction;
+    }
+
+    // Return the field of view of the camera
+    public int getFOV() {
+        return fieldOfView;
     }
 
     // Set camera position
-    public void setPosition(Vector3 vector3) {
-
+    public void setPosition(Vector3 vector3_) {
+        position = vector3_;
     }
 
     // Set camera look direction
-    public void setDirection(Vector3 vector3) {
-
+    public void setDirection(Vector3 vector3_) {
+        direction = vector3_;
     }
+
+    // Set the field of view of the camera
+    public void setFOV(int fov) {
+        fieldOfView = fov;
+    }
+
 }
