965dee7ccbef3effee713c229a9117e805b14bfe
kajatrijsburg <kajatrijsburg@gmail.com>
2022-10-02 13:14:32 +0200

    added comments to the new and updated methods

--- src/test/maths/RayTest.java
+++ src/test/maths/RayTest.java
@@ -8,24 +8,31 @@ import main.maths.Vector3;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RayTest {
+
+    //these tests check if Ray.getPointAtDistance return the expected point.
     @Test
-    void getDistanceAtPoint1() {
+    void getPointAtDistance1() {
+        //make a new ray
         Ray testRay = new Ray(-1, 0, 0, 4, 0, 0);
 
+        //get a point a certain distance from the origin along the ray
         Vector3 point = testRay.getPointAlongRay(10);
 
+        //check if the point is where we expect it to be
         assertEquals(-6.0, point.getX());
         assertEquals(0.0, point.getY());
         assertEquals(0.0, point.getZ());
     }
 
     @Test
-    void getDistanceAtPoint2() {
+    void getPointAtDistance2() {
+        //make a new ray
         Ray testRay = new Ray(0, -1, 0, 4, 1, 2);
 
+        //get a point a certain distance from the origin along the ray
         Vector3 point = testRay.getPointAlongRay(3);
 
-        System.out.println(point.toString());
+        //check if the point is where we expect it to be
         assertEquals(4.0, point.getX());
         assertEquals(-2.0, point.getY());
         assertEquals(2.0, point.getZ());
