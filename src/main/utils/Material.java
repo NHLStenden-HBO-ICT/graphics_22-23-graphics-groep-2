9b0758460e2963c2f9bc2519037c1e8e49ac1817
Job Noordenbos <job.noordenbos@student.nhlstenden.com>
2022-10-03 13:06:26 +0200

    Fused Color.java with Material.java Added functional code to getters and setters in Material Added another vscode specific folder to the gitignore Added a comment to gitignore

--- src/main/utils/Material.java
+++ src/main/utils/Material.java
@@ -1,25 +1,27 @@
 package main.utils;
 
+import main.maths.Vector3;
+
 public class Material {
 
     private double reflectivity;
 
-    private Color color;
+    private Vector3 color;
 
-    public Color getColor() {
-        return null;
+    public Vector3 getColor() {
+        return color;
     }
 
     public double getReflectivity() {
-        return 0;
+        return reflectivity;
     }
 
     public void setColor(Color color) {
-
+        this.color = color;
     }
 
     public void setReflectivity(double reflectivity) {
-
+        this.reflectivity = reflectivity;
     }
 
 }
