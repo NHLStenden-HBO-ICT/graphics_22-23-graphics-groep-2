26531689014dc5eb61f4b09eeaf5c7f1d8711470
Job Noordenbos <job.noordenbos@student.nhlstenden.com>
2022-10-04 13:54:35 +0200

    Added Metallic and Matte to Material.java Also added getters and setters

--- src/main/utils/Material.java
+++ src/main/utils/Material.java
@@ -6,6 +6,10 @@ public class Material {
 
     private Color color;
 
+    private double metallic;
+
+    private double matte;
+
     public Color getColor() {
         return color;
     }
@@ -14,12 +18,28 @@ public class Material {
         return reflectivity;
     }
 
+    public double getMettalic(){
+        return metallic;
+    }
+
+    public double getMatte(){
+        return matte;
+    }
+
     public void setColor(Color color) {
         this.color = color;
     }
 
     public void setReflectivity(double reflectivity) {
         this.reflectivity = reflectivity;
+    }
+
+    public void setMetallic(double metallic){
+        this.metallic = metallic;
+    }
+
+    public void setMatte(double matte){
+        this.matte = matte;
     }
 
 }
