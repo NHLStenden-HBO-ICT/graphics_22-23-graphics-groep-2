597748f57aef546e442a91fd549f8f22591e3072
Job Noordenbos <job.noordenbos@student.nhlstenden.com>
2022-10-04 21:19:21 +0200

    Fixed more errors in the tests caused by the new constructors

--- src/test/maths/ShadowRayTest.java
+++ src/test/maths/ShadowRayTest.java
@@ -4,6 +4,7 @@ import main.geometry.Intersectable;
 import main.geometry.Sphere;
 import main.maths.ShadowRay;
 import main.maths.Vector3;
+import main.utils.Color;
 import main.utils.Material;
 import org.junit.jupiter.api.Test;
 
@@ -47,7 +48,7 @@ class ShadowRayTest {
     @Test
     void castTest() {
         //create an array with two spheres
-        Material mat = new Material();
+        Material mat = new Material(new Color(new Vector3(0,0,0)),0.0,0.0);
 
         Sphere sphereA = new Sphere(mat, 1.0, new Vector3(0, 0, 0));
         Sphere sphereB = new Sphere(mat, 1.0, new Vector3(2, 0, 0));
@@ -67,7 +68,7 @@ class ShadowRayTest {
     @Test
     void castTestMiss() {
         //create an array with two spheres
-        Material mat = new Material();
+        Material mat = new Material(new Color(new Vector3(0,0,0)),0.0,0.0);
 
         Sphere sphereA = new Sphere(mat, 1.0, new Vector3(0, 0, 0));
         Sphere sphereB = new Sphere(mat, 1.0, new Vector3(2, 0, 0));
