5a60ce8828bd939efb233a4f3b6691be27e3e953
Job Noordenbos <job.noordenbos@student.nhlstenden.com>
2022-10-05 15:29:23 +0200

    Added comments to Renderer

--- src/main/rendering/Renderer.java
+++ src/main/rendering/Renderer.java
@@ -32,6 +32,10 @@ public class Renderer {
         scene = scene_;
     }
 
+    // Cast rays from the camera into the scene to detect intersections and see if they need to be lit up
+    // First get screen resolution and ratio
+    // Then setip a buffer and a camera
+    // Then cast a ray through each pixel and calculate the color for it if it intersects with a solid
     public void RenderToImage() {
 
         PixelData pixelData = new PixelData(400, scene.getCamera().getRatio());
@@ -39,13 +43,16 @@ public class Renderer {
 
         Camera camera = scene.getCamera();
 
-        //loop for each pixel on the image
+        // Loop for each pixel on the image
         for (int y = 0; y < pixelData.getHeight(); ++y) {
             for (int x = 0; x < pixelData.getWidth(); ++x) {
 
+                // Cast ray and check if it intersects with something
                 FullRay ray = camera.rayThroughPixel(x, y, pixelData.getWidth(), pixelData.getHeight());//gets the ray with the coörds of the virtual screen that's equal to the x and y pixel of the image
                 RayHit hit = ray.castRay(scene.getGeometry());
 
+                // If the ray intersects with something, write coordinates in console and call the calculateLight(RayHit hit) method with the current rayhit
+                // Finally set that pixel with the final color in the image buffer
                 if (hit != null) {
 
                     System.out.println("intersect on  x: " + x + " y : " + y);
@@ -60,43 +67,66 @@ public class Renderer {
             }
         }
 
+        // At the end call toImage(bufferedImage image) in pixelData to convert the buffer to an image
         pixelData.toImage(buffer);
 
     }
 
+    // This method checks wheter or not a pixel should be lit up by casting a shadow ray to each light source
+    // First it puts the lights and contactpoint in variables and makes a variable for the final output
     public Color calculateLight(RayHit hit) {
         List<PointLight> lights  = scene.getLights();
         Vector3 hitPos = hit.getContactPoint();
         Color finalColor = new Color(0,0,0);
+
+        // Then calculate the light for the given contactpoint for each light and check if it can see the light
         for (int i = 0; i < lights.size(); i++){
 
+            // Get the light position and the direction towards the light
             PointLight light = lights.get(i);
             Vector3 lightPos = light.getPosition();
             Vector3 lightDir = lightPos.sub(hitPos).normalise();
 
+            // Cast a shadowray to the light source
             ShadowRay shaRay = new ShadowRay(lightDir, hitPos);
             boolean lightHit = shaRay.castRay(scene.getGeometry());
 
+            // If it doesn't intersect with anything calculate light and color
+            // Else return black color
             if (lightHit != true){
                 
+                // First calculate the intensity of the color of the pixel according to the intensity of the lightsource and the distance of the lightsource to the contactpoint
                 double lightIntensity = light.getIntensity() / Math.pow(hitPos.distance(lightPos), 2);
+
+                // Get contactpoint solid, get material of that then get the color of it and then get the color of that, divide it by 255 to get 0-1 rgb values
+                // Then multiply it by the light intensity
+                // Then do the same for the color of the light and add the result to the reflection vector
                 Vector3 reflection = hit.getHitSolid().getMaterial().getColor().getColor().divide(255).multi(lightIntensity).add(light.getColor().getColor().divide(255).multi(lightIntensity));
 
+                // Calculate the angle at which the light hits the contactpoint
                 Vector3 lightAngleDir = hitPos.sub(lightPos).normalise();
                 double lightAngle = Math.cos(hitPos.dot(lightAngleDir));
+
+                // Divide the reflection by the angle of the light and multiply it by 255 to get 255 rgb values again
                 Vector3 reflectionFinal = reflection.divide(lightAngle).multi(255);
                 
+                // Clamp the color to a range of 0-255 to prevent rgb overflows
                 Color processedColor = clampColor(reflectionFinal);                
+                
+                // Add the result of the list loop with to the result of this loop
+                // This only happens when there are multiple lights
                 finalColor = addColors(finalColor, processedColor);
             }else{
+                // If there is a hit return black
                 return new Color(0,0,0);
             }
         }
 
+        // Finally return the final result after all the lights have been calculated
         return finalColor;
-
     }
 
+    // Quick method to add the rbg values of two java.awt.Color objects to eachother 
     public Color addColors(Color color1, Color color2){
         int r = Math.min(255, (color1.getRed() + color2.getRed()));
         int g = Math.min(255, (color1.getGreen() + color2.getGreen()));
@@ -104,11 +134,18 @@ public class Renderer {
         return new Color(r,g,b);
     }
 
+    // Method that sets the xyz of the given vector to 255 if they are above that and/or to 0 if they are below that
+    // Outputs a java.awt.Color object
+    // This method works perfectely fine but only works for a vector input and a color output
+    // Could be made dynamic in the future
     public Color clampColor(Vector3 color) {
+
+        // Get values
         double r = color.getX();
         double g = color.getY();
         double b = color.getZ();
 
+        //Check if they are above or below the minimum
         if (r > 255){
             r = 255;
         }
@@ -133,6 +170,7 @@ public class Renderer {
             b = 0;
         }
 
+        // Return the resulting color
         return new Color((int)r,(int)g,(int)b);
     }
 }
