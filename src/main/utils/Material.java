63b98f4bf5dc23dc5308c639740cbb9ce96df5eb
Job Noordenbos <job.noordenbos@student.nhlstenden.com>
2022-10-18 13:51:47 +0200

    Added some extra notes to Material

--- src/main/utils/Material.java
+++ src/main/utils/Material.java
@@ -2,13 +2,28 @@ package main.utils;
 
 public class Material {
 
+    // Note: This value can't be higher than 1 or lower than 0
+    // This is accounted for in the constructor so entering a wrong value won't
+    // break the program
     private double reflectivity;
 
     private VectorColor vectorColor;
 
     public Material(VectorColor vectorColor, double reflectivity) {
         this.vectorColor = vectorColor;
-        this.reflectivity = reflectivity;
+
+        // Check if reflectivity value is above or below the max or min value
+        if (reflectivity > 1.0) {
+            this.reflectivity = 1.0;
+        } else {
+            this.reflectivity = reflectivity;
+        }
+
+        if (reflectivity < 0.0) {
+            this.reflectivity = 0.0;
+        } else {
+            this.reflectivity = reflectivity;
+        }
     }
 
     public VectorColor getColor() {
