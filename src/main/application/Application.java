15703bc00d5023714e4d9624206190cc38c0a6eb
kajatrijsburg <kajatrijsburg@gmail.com>
2022-10-21 16:50:30 +0200

    cleans up the application class

--- src/main/application/Application.java
+++ src/main/application/Application.java
@@ -1,65 +1,25 @@
 package main.application;
 
-import main.geometry.Model;
-import main.geometry.ModelLoader;
-import main.maths.Vector3;
 import main.rendering.Renderer;
-import main.scene.Camera;
-import main.scene.PointLight;
 import main.scene.Scene;
-import main.utils.VectorColor;
 
-import java.io.File;
+import static javax.swing.SwingUtilities.invokeLater;
 
 public class Application {
-
-    private static Renderer renderer;
-    private static Thread thread;
-
     private static int height = 400;
     private static double ratio = 16.0 / 9.0;
-    private static Window window;
+
+    private static int width = (int) (ratio * height);
+    private static MainWindow program;
 
     public static void main(String[] args) {
-        renderer = new Renderer(sceneSetup());
-        window = new Window(height, ratio, renderer);
-        window.start();
-    }
+        //use invoke later to schedule a task on the event dispatch thread (EDT)
+        //swing elements should only be created and modified on the EDT
+        //otherwise unexpected behavior might occur
+        //but because swing is weird unexpected behavior might still occur
+        invokeLater(() -> {
+            program = new MainWindow(height, width);
+        });
 
-
-    //scenesetup method will contain al the setup for the scene and all the items add to said scene so the main method stays small and clean.
-    public static Scene sceneSetup() {
-        //make a new scene
-        Scene scene = new Scene();
-
-        //adds camera to scene
-        scene.setCamera(new Camera(new Vector3(0, 0, 0), height, (int) (height * ratio), 90));
-
-        //adds light to scene
-        scene.addLight(new PointLight(new VectorColor(new Vector3(255, 255, 255)), 500, new Vector3(0, 100, 50)));
-
-        //adds some basic geometry to the scene
-        //scene.addIntersectable(new Sphere(new Material(new VectorColor(new Vector3(255, 0, 0)), 0.0, 0.0), 5, new Vector3(0, -10, -30))); //test sphere
-        //scene.addIntersectable(new Sphere(new Material(new VectorColor(new Vector3(0, 255, 0)), 0.0, 0.0), 10, new Vector3(-10, 10, -60))); //test sphere
-        //scene.addIntersectable(new Triangle(new Material(new VectorColor(new Vector3(255, 255, 255)), 0.0, 0.0), new Vector3(3, 0, -5), new Vector3(-3, 0, -5), new Vector3(0, 3, -5), new Vector3(0, 0, 1)));
-
-        //creates a model loader
-        ModelLoader modelloader = new ModelLoader();
-
-        //creates the modelfile with path that is going to be read, which could end up becoming a list of items.
-        //in our case all object will be put in the folder called objfiles to keep everything clean and tidy
-        File modelfile = new File("objfiles/cup.obj");
-        try {
-            //adds model to scene using the modelloader, it gets the file path and sets the start position
-            Model model = modelloader.readFile(modelfile, (new Vector3(100, 0, -500)));
-            model.setSize(100);
-            scene.addIntersectable(model);
-        } catch (Exception e) {
-            //because a file is being read, for safety it needs a try and catch
-            throw new RuntimeException(e);
-        }
-
-
-        return scene;
     }
 }
