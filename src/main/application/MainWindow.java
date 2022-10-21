622728f0b784b623e53379f8809d7698a13ed468
kajatrijsburg <kajatrijsburg@gmail.com>
2022-10-21 17:00:57 +0200

    cleans up MainWindow and adds comments

--- src/main/application/MainWindow.java
+++ src/main/application/MainWindow.java
@@ -1,7 +1,6 @@
 package main.application;
 
 import main.rendering.RenderTask;
-import main.rendering.Renderer;
 import main.scene.Scene;
 import main.scene.SceneFactory;
 
@@ -14,19 +13,13 @@ public class MainWindow extends JFrame implements EventListener {
 
     //ray tracing stuff
     private Scene scene;
-    private Renderer renderer;
 
     //UI elements
-    private JLayeredPane layeredPane;
     private StatusBar statusBar;
     private JPanel actionBar;
     private JPanel ui;
     private Button renderButton;
     private JLabel imageLabel;
-
-    public BufferedImage getImage() {
-        return image;
-    }
 
     public void setImage(BufferedImage image) {
         //safe the image in a local variable
@@ -52,43 +45,46 @@ public class MainWindow extends JFrame implements EventListener {
         setVisible(true);
         setLayout(null);
         setBackground(Color.BLACK);
+
+        //set the layout for our window
+        getLayeredPane().setLayout(new BorderLayout());
+        //make the layered pane not render background
+        getLayeredPane().setOpaque(false);
     }
 
     private void configureUI() {
-        this.getLayeredPane().setLayout(new BorderLayout());
-        this.getLayeredPane().setOpaque(false);
-
-        //image
+        //make a new JLabel to display our image in and configure it
         imageLabel = new JLabel();
         imageLabel.setBackground(Color.BLACK);
         imageLabel.setBounds(0, 0, this.getLayeredPane().getWidth(), this.getLayeredPane().getHeight());
         this.getLayeredPane().add(imageLabel, BorderLayout.CENTER);
         this.getLayeredPane().setLayer(imageLabel, JLayeredPane.DEFAULT_LAYER);
 
-        //make two different panels for the separate UI elements & a panel to contain both
+        //make two different panels for the separate UI elements (actionBar & statusBar)
+        // & a panel to contain both (ui)
+        //configure the ui JPanel. This panel contains the action bar and the status bar
         ui = new JPanel();
-        statusBar = new StatusBar();
-        actionBar = new JPanel();
-
-        //configure the bars
         ui.setBounds(0, 0, this.getContentPane().getWidth(), this.getContentPane().getHeight());
         ui.setLayout(new BorderLayout());
         ui.setOpaque(false);
-        //ui.setBackground(Color.RED);
 
+        //statusBar is its own class that extends JPanel
+        //it handles its own configuration and a number of other tasks
+        statusBar = new StatusBar();
+
+        //configure the action bar
+        actionBar = new JPanel();
         actionBar.setOpaque(false);
         actionBar.setLayout(new FlowLayout(FlowLayout.LEFT));
 
-        //instantiate UI elements
-        renderButton = new Button("Render");
 
-        //configure the UI elements
-        //render button
+        //instance a new button. This button will render our scene when clicked
+        renderButton = new Button("Render");
         renderButton.setPreferredSize(new Dimension(70, 20));
         renderButton.addActionListener(e -> render());
 
 
-        //add the UI elements to the appropriate bars
+        //add the button to the appropriate bar
         actionBar.add(renderButton);
 
         //add the bars to the ui panel
@@ -100,25 +96,31 @@ public class MainWindow extends JFrame implements EventListener {
         this.getLayeredPane().setLayer(ui, JLayeredPane.PALETTE_LAYER);
 
         //set everything to visible
+        //if this isn't called unexpected behavior and weird graphical glitches might occur
         this.setVisible(true);
     }
 
+    //sets up everything, so we're ready to render a scene.
     private void setUpRayTracer() {
         statusBar.setState(States.LOADING_MODEL);
         scene = SceneFactory.modelScene(getHeight(), getWidth());
         statusBar.setState(States.IDLE);
     }
 
+    //when this is called a scene is rendered
     private void render() {
         int width, height;
         width = (int) scene.getCamera().getImageWidth();
         height = (int) scene.getCamera().getImageHeight();
 
+        //makes a new render task and executes it
         new RenderTask(this, height, width).execute();
     }
 
-    public Renderer getRenderer() {
-        return renderer;
+
+    //some getters and setters
+    public BufferedImage getImage() {
+        return image;
     }
 
     public Scene getScene() {
