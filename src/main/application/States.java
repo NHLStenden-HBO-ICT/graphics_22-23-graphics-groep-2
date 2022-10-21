3f1371bace17efb1443cc0bf2dc426c82fdf3567
kajatrijsburg <kajatrijsburg@gmail.com>
2022-10-21 16:47:55 +0200

    offloads some code to seperate classes to reduce the size of MainWindow

---
+++ src/main/application/States.java
@@ -0,0 +1,9 @@
+package main.application;
+
+//program states
+public enum States {
+    STARTING,       //starting up the program
+    LOADING_MODEL,  //loading a model
+    RENDERING,      //rendering
+    IDLE            //doing nothing
+}
---
+++ src/main/application/StatusBar.java
@@ -0,0 +1,70 @@
+package main.application;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StatusBar extends JPanel {
+
+    States currentState;
+
+    private Label stateDisplay;
+    private JProgressBar progressBar;
+
+    //this method should only be called from the event dispatch thread because it affects the gui
+    public StatusBar() {
+        this.setOpaque(false);
+        this.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        stateDisplay = new Label();
+        progressBar = new JProgressBar();
+
+        //state display label
+        stateDisplay.setPreferredSize(new Dimension(100, 20));
+
+        //progress bar
+        progressBar.setPreferredSize(new Dimension(100, 20));
+
+        this.add(stateDisplay);
+        this.add(progressBar);
+        this.setVisible(true);
+    }
+
+    public Label getStateDisplay() {
+        return stateDisplay;
+    }
+
+    public JProgressBar getProgressBar() {
+        return progressBar;
+    }
+
+    //this method modifies the GUI and should therefore only be called from the event dispatch thread
+    public void setProgressMinMax(int min, int max) {
+        progressBar.setMinimum(min);
+        progressBar.setMaximum(max);
+    }
+
+    //takes a state and updates the ui to be correct
+    //this method modifies the gui and should therefore only be called from the event dispatch thread
+    public void setState(States state) {
+        currentState = state;
+        progressBar.setVisible(false);
+
+        switch (currentState) {
+            case STARTING -> stateDisplay.setText("Starting...");
+
+            case LOADING_MODEL -> stateDisplay.setText("Loading a model...");
+
+            case IDLE -> stateDisplay.setText("Awaiting input.");
+
+            case RENDERING -> {
+                stateDisplay.setText("Rendering...");
+                progressBar.setVisible(true);
+            }
+        }
+
+    }
+
+    public void setProgress(int value) {
+        progressBar.setValue(value);
+    }
+}
